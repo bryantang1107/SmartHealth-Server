2876e404f5da433a2cb96e47dec87d2dc61fb64e
bryantang1107 <bryantang10@gmail.com>
2022-05-23 16:08:30 +0800

    Latest Commit

--- firebase.js
+++ firebase.js
@@ -3,10 +3,9 @@
 import admin from "firebase-admin";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const serviceAccount = require("./serviceAccount.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(JSON.parse(process.env.SERVICE_ACCOUNT)),
 });
 
 export default admin; //admin used to authenticate user instead of using jwt

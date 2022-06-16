4d7774cdb73482552bcc4f41aefb229fbc949067
bryantang1107 <bryantang10@gmail.com>
2022-06-16 20:11:15 +0800

    Latest Commit

--- routes/appointment.js
+++ routes/appointment.js
@@ -35,6 +35,7 @@ router.post("/joinroom/:id", async (req, res) => {
     await appointment.save();
     res.status(200).send("ok");
   } catch (error) {
+    console.log(error);
     res.status(500).send("Internal Server Error");
   }
 });
